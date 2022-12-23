742a40657b5bbb43b7aea77bbe321f0d737b438b
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-12-23 20:25:40 +0800

    added hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,28 @@ window.addEventListener("scroll", () => {
     : body.classList.add("scroll-up");
 
   lastScroll = currentScroll;
+});
 
-  console.log(currentScroll);
+const hamburgerMenu = document.querySelector(".ham");
+
+hamburgerMenu.addEventListener("click", () => {
+  const navList = document.querySelector(".nav-list");
+
+  if (!hamburgerMenu.classList.contains("active")) {
+    navList.style.cssText = "transform: translateY(-100%)";
+    document.body.style.overflow = "visible";
+  } else {
+    navList.style.cssText = "transform: translateY(13rem)";
+    document.body.style.overflow = "hidden";
+  }
+
+  const navListItem = document.querySelectorAll(".nav-list-item");
+
+  navListItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      navList.style.cssText = "transform: translateY(-100%)";
+      hamburgerMenu.classList.remove("active");
+      document.body.style.overflow = "visible";
+    });
+  });
 });
