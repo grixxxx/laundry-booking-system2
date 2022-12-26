25507a3a3f4f7a63aec60730c61976aec7d3fd80
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-12-26 21:52:42 +0800

    added postcss

---
+++ dist/index.09c24910.js
@@ -0,0 +1,34 @@
+const body = document.body;
+let lastScroll = 0;
+window.addEventListener("scroll", ()=>{
+    const currentScroll = window.pageYOffset;
+    currentScroll === 0 ? body.classList.remove("scroll-up") : body.classList.add("scroll-up");
+    if (currentScroll <= lastScroll) body.classList.remove("scroll-up");
+    lastScroll = currentScroll;
+});
+const hamburgerMenu = document.querySelector(".ham");
+hamburgerMenu.addEventListener("click", ()=>{
+    const navList = document.querySelector(".nav-list");
+    const navListItem = document.querySelectorAll(".nav-list-item");
+    if (!hamburgerMenu.classList.contains("active")) navList.style.cssText = "none";
+    else navList.style.cssText = "transform: translateY(13rem);";
+    navListItem.forEach((item)=>{
+        item.addEventListener("click", ()=>{
+            navList.style.cssText = "none";
+            hamburgerMenu.classList.remove("active");
+        });
+    });
+});
+const loginModal = document.querySelector(".login-modal");
+setInterval(()=>{
+    loginModal.hasAttribute("open") ? document.body.style.overflow = "hidden" : document.body.style.overflow = "visible";
+}, 500);
+const loginButton = document.querySelector(".login-button").addEventListener("click", ()=>{
+    loginModal.showModal();
+});
+const signupModal = document.querySelector(".signup-modal");
+const signupButton = document.querySelector(".signup-button ").addEventListener("click", ()=>{
+    signupModal.showModal();
+});
+
+//# sourceMappingURL=index.09c24910.js.map
