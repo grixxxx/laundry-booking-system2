a5590ec03c91d268049f2da98c7b1aa1b35d6231
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-12-28 23:04:12 +0800

    added dashboard design

--- dist/index.09c24910.js
+++ dist/index.09c24910.js
@@ -1,3 +1,6 @@
+var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+if (a.indexOf("html") > -1) //Check of html String in URL.
+url = url.substring(0, newURL.lastIndexOf("."));
 const body = document.body;
 let lastScroll = 0;
 window.addEventListener("scroll", ()=>{
